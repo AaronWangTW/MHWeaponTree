84f828285e4bb425f1522de14d623baa3e430cd2
AaronWangTW <honglin2023@gmail.com>
2025-06-14 23:07:06 +0800

    basic node display done

---
+++ weaponapp/app/lib/infoReader.js
@@ -0,0 +1,38 @@
+export function readInfo() {
+    const data = require('./weapon_info.json');
+    
+    var all_nodes = [];
+    var all_edges = [];
+
+    for(let i = 0; i < 14; i++) {
+        var cnt = 0;
+        var id_t = 0;
+        var nodes = [];
+        var edges = [];
+        for(const name in data[i]) {
+            nodes.push({
+                id:name,
+                position: {x:i*300,y:cnt*50},
+                data: {label: name},
+                type:'weapon'
+            })
+            for(const up of data[i][name]['upgrades']) {
+                edges.push({
+                    id:id_t,
+                    source: name,
+                    target: up
+                })
+                id_t++;
+            }
+            
+            cnt++;
+        }
+        all_nodes.push(nodes);
+        all_edges.push(edges);
+    }
+
+    return {
+        nodes: all_nodes,
+        edges: all_edges
+    };
+};
---
+++ weaponapp/app/page.js
@@ -0,0 +1,95 @@
+// app/page.js
+'use client';
+
+import {
+  ReactFlow,
+  ReactFlowProvider,
+  Panel,
+  useNodesState,
+  useEdgesState,
+  useReactFlow,
+} from '@xyflow/react';
+import '@xyflow/react/dist/style.css';
+import { readInfo } from './lib/infoReader';
+import Dagre from '@dagrejs/dagre';
+import React, { useCallback,useEffect,useMemo } from 'react';
+import { WeaponNode } from './components/weapon-tree/weaponNode';
+
+
+const info = readInfo();
+const all_nodes = info.nodes
+const all_edges = info.edges
+
+const getLayoutedElements = (nodes, edges) => {
+  const g = new Dagre.graphlib.Graph().setDefaultEdgeLabel(() => ({}));
+  g.setGraph({ rankdir: 'LR' });
+
+  edges.forEach((edge) => g.setEdge(edge.source, edge.target));
+  nodes.forEach((node) =>
+    g.setNode(node.id, {
+      ...node,
+      width: node.measured?.width ?? 0,
+      height: node.measured?.height ?? 0,
+    }),
+  );
+
+  Dagre.layout(g);
+
+  return {
+    nodes: nodes.map((node) => {
+      const position = g.node(node.id);
+      // We are shifting the dagre node position (anchor=center center) to the top left
+      // so it matches the React Flow node anchor point (top left).
+      const x = position.x - (node.measured?.width ?? 0) / 2;
+      const y = position.y - (node.measured?.height ?? 0) / 2;
+
+      return { ...node, position: { x, y } };
+    }),
+    edges,
+  };
+};
+
+const useLayout = () => {
+  const { fitView } = useReactFlow();
+  const [nodes, setNodes, onNodesChange] = useNodesState(all_nodes[5]);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(all_edges[5]);
+  const layouted = getLayoutedElements(nodes, edges);
+
+  setNodes([...layouted.nodes]);
+  setEdges([...layouted.edges]);
+
+  fitView();
+}
+
+const LayoutFlow = () => {
+  const [nodes, setNodes, onNodesChange] = useNodesState(all_nodes[5]);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(all_edges[5]);
+  const nodeTypes = useMemo(() => ({
+    weapon: WeaponNode
+  }, []))
+
+  return (
+    <ReactFlow
+      nodes={nodes}
+      edges={edges}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      nodeTypes={nodeTypes}
+      fitView
+    >
+    </ReactFlow>
+  );
+};
+
+export default function Home() {
+  useEffect(() => {
+    useLayout();
+  });
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <ReactFlowProvider>
+        <LayoutFlow />
+      </ReactFlowProvider>
+    </div>
+  );
+}
