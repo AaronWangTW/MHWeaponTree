313dca72da5d2896d5820ee3102049170251f03a
AaronWangTW <honglin2023@gmail.com>
2025-06-19 20:30:34 +0800

    asset path adjustment

--- weaponapp/next.config.mjs
+++ weaponapp/next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
     basePath: "/weaponapp",
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
+    assetPrefix: '/weaponapp',
 };
 
 export default nextConfig;
